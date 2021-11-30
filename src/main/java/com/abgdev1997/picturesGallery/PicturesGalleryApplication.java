d0d68e5a4667fa742dba56da418f67c6c2ef1063
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-30 13:33:23 +0100

    Cloudinary dependencie and gitignore added

---
+++ src/main/java/com/abgdev1997/picturesGallery/PicturesGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.abgdev1997.picturesGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicturesGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicturesGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abgdev1997/picturesGallery/PicturesGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abgdev1997.picturesGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicturesGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
