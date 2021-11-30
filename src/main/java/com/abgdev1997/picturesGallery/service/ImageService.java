9ecaf0a715411ccbd7cc24e60e2e1b8dfb557eaf
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-30 15:31:42 +0100

    ImageService -> list

---
+++ src/main/java/com/abgdev1997/picturesGallery/service/ImageService.java
@@ -0,0 +1,27 @@
+package com.abgdev1997.picturesGallery.service;
+
+import com.abgdev1997.picturesGallery.entity.Image;
+import com.abgdev1997.picturesGallery.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ImageService {
+
+    @Autowired
+    ImageRepository imageRepository;
+
+    /**
+     * Find a list with all the images
+     * @return
+     */
+    public List<Image> list(){
+        return imageRepository.findByOrderById();
+    }
+
+
+}
