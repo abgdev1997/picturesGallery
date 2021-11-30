0fbdf71264217d2e94c413325f02eeed8b098d45
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-30 15:20:56 +0100

    Message if image isn't a image

--- src/main/java/com/abgdev1997/picturesGallery/controller/CloudinaryController.java
+++ src/main/java/com/abgdev1997/picturesGallery/controller/CloudinaryController.java
@@ -1,5 +1,6 @@
 package com.abgdev1997.picturesGallery.controller;
 
+import com.abgdev1997.picturesGallery.dto.MessageDto;
 import com.abgdev1997.picturesGallery.service.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,7 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 @RestController
@@ -26,6 +30,11 @@ public class CloudinaryController {
      */
     @PostMapping("/upload")
     public ResponseEntity<Map> upload(@RequestParam MultipartFile multipartFile) throws IOException {
+        InputStream is;
+        BufferedImage bi = ImageIO.read(multipartFile.getInputStream());
+        if(bi == null) {
+            return new ResponseEntity(new MessageDto("This isn't a image!"), HttpStatus.BAD_REQUEST);
+        }
         Map result = cloudinaryService.upload(multipartFile);
         return new ResponseEntity(result, HttpStatus.OK);
     }
