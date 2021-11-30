d9a23e6d78918cf6d2c84c29e0d4da4477911539
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-30 15:27:12 +0100

    ImageRepository class added

---
+++ src/main/java/com/abgdev1997/picturesGallery/repository/ImageRepository.java
@@ -0,0 +1,13 @@
+package com.abgdev1997.picturesGallery.repository;
+
+
+import com.abgdev1997.picturesGallery.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findByOrderById();
+}
