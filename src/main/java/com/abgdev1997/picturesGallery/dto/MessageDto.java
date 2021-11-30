9fbb4c50565cac51f8bf4575252b58753efe5f0b
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-30 15:16:57 +0100

    MessageDto added

---
+++ src/main/java/com/abgdev1997/picturesGallery/dto/MessageDto.java
@@ -0,0 +1,18 @@
+package com.abgdev1997.picturesGallery.dto;
+
+public class MessageDto {
+
+    String message;
+
+    public MessageDto(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
